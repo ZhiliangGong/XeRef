44fabcbc1245bb6bb57892f3c69d6e0c0f59ca2e
Zhiliang Gong <zhilianggong@nsit-dhcp-035-172.uchicago.edu>
2017-04-27 10:22:12 -0500

    lee mac fits

--- quick_start.m
+++ quick_start.m
@@ -4,9 +4,12 @@ files = {'g20_30ps_1.ref', 'g20_30ps_mfeg8_1.ref', 'g25_30ps_mfeg8_1.ref', 'g30_
 this = XeRef(files);
 % pdbfile = 'mfge8_trial1_protonly.pdb';
 % pdbfile = 'hmmm-end-frame-protien.pdb';
-% pdbfile = 'mfge8_trial5bfearlyf199.pdb';
-pdbfile = 'mfge8_trial5bflatef199.pdb';
+pdbfile = 'mfge8_trial5bfearlyf199.pdb';
+% pdbfile = 'mfge8_trial5bflatef199.pdb';
 this.control('load-pdb', pdbfile);
 a = this.data{1};
 s = this.layers;
 % this.layers.protein.contourHeight();
+
+% load parameters_0425.mat;
+% this.gui.parametersTable.Data = parameters_0425;
